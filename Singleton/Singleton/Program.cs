65aa3f5fdb804078e49bf5315dcd46174418d34d
warraich81 <adeelahmed.warraich@gmail.com>
2017-10-06 21:36:00 +0500

    add project files

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                ()=> printEmp() ,
+                ()=> printStudent()
+                );
+          
+            Console.ReadLine();
+        }
+
+        private static void printStudent()
+        {
+            Singleton s3 = Singleton.GetInstance;
+            s3.printMsg("Welcome to DP");
+        }
+
+        private static void printEmp()
+        {
+            Singleton s = Singleton.GetInstance;
+            s.printMsg("Hello World");
+        }
+    }
+}
---
+++ Singleton/Singleton/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Singleton
+{
+   public sealed class Singleton
+    {
+        private static int instanceCount = 0;
+
+        //private static readonly  Singleton instance = new Singleton (); //Eager Loading
+        private static readonly Lazy<Singleton> instance = new Lazy<Singleton>( ()=> new Singleton() );  //Lazy Loading
+        private Singleton()
+        {
+            instanceCount ++; 
+            Console.WriteLine("Instance count = " + instanceCount.ToString());
+        }
+
+   public static Singleton GetInstance
+        {
+            get
+            {
+                //return instance; //Eager Loading
+                return instance.Value; //Lazy Loading
+            }
+        }
+        public void printMsg(string msg)
+        {
+
+            Console.WriteLine(msg);
+        }
+    }
+}
